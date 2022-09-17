28095f1b5f66f2cfda89a041f6e0545e737dcdfb
dtyghc <100001963+dtyghc@users.noreply.github.com>
2022-09-17 20:30:12 +0530

    Catuserbot: Added new features
    Co-Authored-By: sandeep.n <58665444+sandy1709@users.noreply.github.com>
    Co-Authored-By: Jisan <66010486+Jisan09@users.noreply.github.com>
    Co-Authored-By: dtyghc <100001963+dtyghc@users.noreply.github.com>

--- userbot/utils/startup.py
+++ userbot/utils/startup.py
@@ -69,9 +69,9 @@ async def startupmessage():
         if BOTLOG:
             Config.CATUBLOGO = await catub.tgbot.send_file(
                 BOTLOG_CHATID,
-                "https://telegra.ph/file/4e3ba8e8f7e535d5a2abe.jpg",
+                "https://telegra.ph/file/40ff9ae622268fa120a06.jpg",
                 caption="**Your CatUserbot has been started successfully.**",
-                buttons=[(Button.url("Support", "https://t.me/catuserbot"),)],
+                buttons=[(Button.url("Support", "https://t.me/Anant_Goel"),)],
             )
     except Exception as e:
         LOGS.error(e)
